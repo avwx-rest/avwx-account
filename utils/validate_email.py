29f3b9226699243966f9c7b041e94773c79d0314
Michael duPont <flyinactor91@gmail.com>
2021-12-09 10:58:02 -0500

    Update email confirm key

--- utils/validate_email.py
+++ utils/validate_email.py
@@ -18,7 +18,7 @@ load_dotenv()
 def change_plan(email: str) -> int:
     """Change a user's plan details"""
     mdb = MongoClient(environ["MONGO_URI"])
-    command = {"$set": {"email_verified_at": datetime.utcnow()}}
+    command = {"$set": {"email_confirmed_at": datetime.utcnow()}}
     resp = mdb.account.user.update_one({"email": email}, command)
     if not resp.matched_count:
         print(f"No user found for {email}")
